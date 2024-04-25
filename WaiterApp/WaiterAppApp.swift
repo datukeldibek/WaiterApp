f72490a7c40e01fda1fede6120444863f4dfa41a
Datkaiym Keldibekova <dkeldibekova08@gmail.com>
2024-04-25 21:44:05 +0600

    Initial Commit

---
+++ WaiterApp/WaiterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaiterAppApp.swift
+//  WaiterApp
+//
+//  Created by Datu on 25/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WaiterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
